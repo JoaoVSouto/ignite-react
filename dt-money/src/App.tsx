f13d2481d7b94e1c2f86296a3cfc35b086326f19
JoaoVSouto <joaovitorss99@hotmail.com>
2021-04-30 16:06:28 -0300

    Create global styles

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,5 +1,12 @@
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1>hello world</h1>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
