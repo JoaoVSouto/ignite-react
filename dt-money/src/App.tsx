f755c8dc56b7b7d24728771b1ffea6a685c44a6f
JoaoVSouto <joaovitorss99@hotmail.com>
2021-05-01 09:28:20 -0300

    Create and style header component

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,9 +1,12 @@
+import Header from './components/Header';
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Header />
+
       <GlobalStyle />
     </>
   );
