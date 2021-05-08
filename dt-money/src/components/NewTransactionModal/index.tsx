e6bbac4564f73e08c481414c0dd67c4f01d918a8
JoaoVSouto <joaovitorss99@hotmail.com>
2021-05-08 11:41:20 -0300

    Create and style transactions type buttons

--- dt-money/src/components/NewTransactionModal/index.tsx
+++ dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 import * as S from './styles';
 
@@ -35,6 +37,17 @@ export default function NewTransactionModal({
         <input placeholder="Título" />
 
         <input type="number" placeholder="Preço" />
+
+        <S.TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </S.TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
