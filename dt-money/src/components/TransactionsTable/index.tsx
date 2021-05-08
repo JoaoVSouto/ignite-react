5e5fcf75765528531d14df2ad7bb2a998322f85f
JoaoVSouto <joaovitorss99@hotmail.com>
2021-05-08 15:32:54 -0300

    Request transactions and populate them on table

--- dt-money/src/components/TransactionsTable/index.tsx
+++ dt-money/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,27 @@
+import * as React from 'react';
+
+import api from '../../services/api';
+
 import * as S from './styles';
 
+type Transaction = {
+  id: string;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+};
+
 export default function TransactionsTable() {
+  const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+
+  React.useEffect(() => {
+    api
+      .get('transactions')
+      .then(response => setTransactions(response.data.transactions));
+  }, []);
+
   return (
     <S.Container>
       <table>
@@ -14,30 +35,14 @@ export default function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td className="title">Desenvolvimento de site</td>
-            <td className="deposit">R$ 12.000,00</td>
-            <td>Venda</td>
-            <td>13/04/2021</td>
-          </tr>
-          <tr>
-            <td className="title">Hamburguer</td>
-            <td className="withdraw">- R$ 59,00</td>
-            <td>Alimentação</td>
-            <td>10/04/2021</td>
-          </tr>
-          <tr>
-            <td className="title">Aluguel do apartamento</td>
-            <td className="withdraw">- R$ 1.200,00</td>
-            <td>Casa</td>
-            <td>27/03/2021</td>
-          </tr>
-          <tr>
-            <td className="title">Computador</td>
-            <td className="deposit">R$ 5.400,00</td>
-            <td>Venda</td>
-            <td>15/03/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td className="title">{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </S.Container>
