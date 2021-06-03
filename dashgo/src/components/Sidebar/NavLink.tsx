22a35a907775f1faf64ae58f1ebe358d884b742c
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-03 15:54:33 -0300

    Add navigation functionality to links

--- dashgo/src/components/Sidebar/NavLink.tsx
+++ dashgo/src/components/Sidebar/NavLink.tsx
@@ -1,17 +1,26 @@
-import { Text, Link, LinkProps, Icon } from '@chakra-ui/react';
+import { Text, Link as ChakraLink, LinkProps, Icon } from '@chakra-ui/react';
+import Link from 'next/link';
 
 type NavLinkProps = {
   icon: React.ElementType;
   children: string;
+  href: string;
 } & LinkProps;
 
-export default function NavLink({ children, icon, ...rest }: NavLinkProps) {
+export default function NavLink({
+  children,
+  icon,
+  href,
+  ...rest
+}: NavLinkProps) {
   return (
-    <Link href="#!" display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize={20} />
-      <Text ml={4} fontWeight="medium">
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize={20} />
+        <Text ml={4} fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
