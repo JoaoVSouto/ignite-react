702aaeb6f196a1ef7470346163ab7b5e259ef9eb
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-03 15:37:00 -0300

    Add responsivity to users listing page

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -12,6 +12,7 @@ import {
   Th,
   Thead,
   Tr,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
@@ -20,6 +21,8 @@ import Sidebar from 'components/Sidebar';
 import Pagination from 'components/Pagination';
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({ base: false, lg: true });
+
   return (
     <Box>
       <Header />
@@ -47,17 +50,16 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px={6} width={8}>
+                <Th px={[4, 4, 6]} width={8}>
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w={8} />
+                {isWideVersion && <Th>Data de cadastro</Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px={6}>
+                <Td px={[4, 4, 6]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -68,62 +70,7 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>02 de Junho, 2021</Td>
-                <Td>
-                  <Button
-                    size="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={6}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Vítor</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joao@vitor.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>02 de Junho, 2021</Td>
-                <Td>
-                  <Button
-                    size="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={6}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Vítor</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      joao@vitor.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>02 de Junho, 2021</Td>
-                <Td>
-                  <Button
-                    size="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>02 de Junho, 2021</Td>}
               </Tr>
             </Tbody>
           </Table>
