e46dc7f98f5ac9df2a2828830b1ee3a5598442e8
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-03 13:37:23 -0300

    Turn Header component responsive

--- dashgo/src/components/Header/Profile.tsx
+++ dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export default function Profile() {
+type ProfileProps = {
+  showProfileData?: boolean;
+};
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr={4} textAlign="right">
-        <Text>João Vítor</Text>
-        <Text color="gray.300" fontSize="small">
-          joao@vitor.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr={4} textAlign="right">
+          <Text>João Vítor</Text>
+          <Text color="gray.300" fontSize="small">
+            joao@vitor.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
