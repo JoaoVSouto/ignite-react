18709655962f553bf8c5c0a16c30bdc323813e05
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-03 11:28:02 -0300

    Extract Pagination component into separate ones

---
+++ dashgo/src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@chakra-ui/react';
+
+type PaginationItemProps = {
+  number: number;
+  isCurrent?: boolean;
+};
+
+export default function PaginationItem({
+  isCurrent = false,
+  number,
+}: PaginationItemProps) {
+  if (isCurrent) {
+    return (
+      <Button
+        size="sm"
+        fontSize="xs"
+        w={4}
+        colorScheme="pink"
+        disabled
+        _disabled={{ bg: 'pink.500', cursor: 'default' }}
+      >
+        {number}
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      size="sm"
+      fontSize="xs"
+      w={4}
+      bgColor="gray.700"
+      _hover={{ bg: 'gray.500' }}
+    >
+      {number}
+    </Button>
+  );
+}
