db345ff2591dc62594fe4772f675baedfb98d54d
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-04 13:35:25 -0300

    Create ActiveLink component

--- dashgo/src/components/Sidebar/NavLink.tsx
+++ dashgo/src/components/Sidebar/NavLink.tsx
@@ -1,5 +1,6 @@
 import { Text, Link as ChakraLink, LinkProps, Icon } from '@chakra-ui/react';
-import Link from 'next/link';
+
+import ActiveLink from 'components/ActiveLink';
 
 type NavLinkProps = {
   icon: React.ElementType;
@@ -14,13 +15,13 @@ export default function NavLink({
   ...rest
 }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" align="center" {...rest}>
         <Icon as={icon} fontSize={20} />
         <Text ml={4} fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
