7e0c496e162ce3d93b598dca7800808fe4af621b
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-07 15:53:29 -0300

    Structure options list

--- world-trip/src/pages/index.tsx
+++ world-trip/src/pages/index.tsx
@@ -1,5 +1,14 @@
 import Head from 'next/head';
-import { Box, Flex, Heading, Img, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Img,
+  ListItem,
+  SimpleGrid,
+  Text,
+  UnorderedList,
+} from '@chakra-ui/react';
 
 export default function Home() {
   return (
@@ -33,6 +42,92 @@ export default function Home() {
           Chegou a hora de tirar do papel a viagem que você sempre sonhou.
         </Text>
       </Box>
+
+      <Flex
+        as={UnorderedList}
+        flexWrap="wrap"
+        justify="center"
+        listStyleType="none"
+        css={{ gap: '1.75rem' }}
+        mx={12}
+        my={9}
+      >
+        <ListItem
+          fontSize="large"
+          fontWeight="medium"
+          _before={{
+            content: '""',
+            bgColor: 'yellow.400',
+            height: '8px',
+            width: '8px',
+            borderRadius: '50%',
+            display: 'inline-block',
+            marginRight: '1rem',
+          }}
+        >
+          vida noturna
+        </ListItem>
+        <ListItem
+          fontSize="large"
+          fontWeight="medium"
+          _before={{
+            content: '""',
+            bgColor: 'yellow.400',
+            height: '8px',
+            width: '8px',
+            borderRadius: '50%',
+            display: 'inline-block',
+            marginRight: '1rem',
+          }}
+        >
+          praia
+        </ListItem>
+        <ListItem
+          fontSize="large"
+          fontWeight="medium"
+          _before={{
+            content: '""',
+            bgColor: 'yellow.400',
+            height: '8px',
+            width: '8px',
+            borderRadius: '50%',
+            display: 'inline-block',
+            marginRight: '1rem',
+          }}
+        >
+          moderno
+        </ListItem>
+        <ListItem
+          fontSize="large"
+          fontWeight="medium"
+          _before={{
+            content: '""',
+            bgColor: 'yellow.400',
+            height: '8px',
+            width: '8px',
+            borderRadius: '50%',
+            display: 'inline-block',
+            marginRight: '1rem',
+          }}
+        >
+          clássico
+        </ListItem>
+        <ListItem
+          fontSize="large"
+          fontWeight="medium"
+          _before={{
+            content: '""',
+            bgColor: 'yellow.400',
+            height: '8px',
+            width: '8px',
+            borderRadius: '50%',
+            display: 'inline-block',
+            marginRight: '1rem',
+          }}
+        >
+          e mais...
+        </ListItem>
+      </Flex>
     </>
   );
 }
