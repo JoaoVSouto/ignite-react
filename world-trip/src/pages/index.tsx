90ca4be6776827c6b00ef92b81e022461adc59f4
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-06 22:00:05 -0300

    Structure header and home banner

--- world-trip/src/pages/index.tsx
+++ world-trip/src/pages/index.tsx
@@ -1,3 +1,38 @@
+import Head from 'next/head';
+import { Box, Flex, Heading, Img, Text } from '@chakra-ui/react';
+
 export default function Home() {
-  return <h1>hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>worldtrip</title>
+      </Head>
+
+      <Flex justify="center" align="center" py={4}>
+        <Img src="/img/logo.svg" alt="world trip" w={20} />
+      </Flex>
+
+      <Box
+        bgImage="/img/banner-bg.jpg"
+        bgRepeat="no-repeat"
+        bgSize="cover"
+        backgroundPosition="center -23vw"
+        px={4}
+        py={7}
+      >
+        <Heading
+          color="gray.50"
+          fontWeight="medium"
+          fontSize="xl"
+          lineHeight={7}
+        >
+          5 Continentes, <br />
+          infinitas possibilidades.
+        </Heading>
+        <Text color="gray.200" fontSize="sm" mt={2} lineHeight={5}>
+          Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+        </Text>
+      </Box>
+    </>
+  );
 }
