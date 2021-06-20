9b6b8a38c15d7542789ad2cc9dd1257f67872fc7
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-20 11:25:38 -0300

    Add mutation for creating a new user

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -9,9 +9,14 @@ import {
   Stack,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useMutation } from 'react-query';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
+
+import api from 'services/api';
+import { queryClient } from 'services/queryClient';
 
 import Input from 'components/Form/Input';
 import Header from 'components/Header';
@@ -37,14 +42,36 @@ const createUserFormSchema = Yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      async onSuccess() {
+        await queryClient.invalidateQueries('users');
+      },
+    }
+  );
+
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm({ resolver: yupResolver(createUserFormSchema) });
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = values => {
-    console.log(values);
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async values => {
+    await createUser.mutateAsync(values);
+
+    router.push('/users');
   };
 
   return (
