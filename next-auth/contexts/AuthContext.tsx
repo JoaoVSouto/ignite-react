596aa48ff7bf02df34e8209fda05410d71e81dc4
João Vítor <joaovsoutosantos@gmail.com>
2021-08-08 22:11:31 -0300

    Store tokens on cookies

--- next-auth/contexts/AuthContext.tsx
+++ next-auth/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { setCookie } from 'nookies';
 import Router from 'next/router';
 
 import { api } from '../services/api';
@@ -41,6 +42,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
       });
 
       const { permissions, roles, token, refreshToken } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+      setCookie(undefined, 'nextauth.refreshtoken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
 
       setUser({
         email,
