5be1cb412025ff27043671e59b77a725cb8c75b1
João Vítor <joaovsoutosantos@gmail.com>
2021-08-23 19:14:30 -0300

    Validate auth state on server & refactor axios client

--- next-auth/pages/dashboard.tsx
+++ next-auth/pages/dashboard.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 
 import { AuthContext } from '../contexts/AuthContext';
+import { setupApiClient } from '../services/api';
+import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
   const { user } = React.useContext(AuthContext);
 
   return <h1>dashboard!: {user?.email}</h1>;
 }
+
+export const getServerSideProps = withSSRAuth(async ctx => {
+  const apiClient = setupApiClient(ctx);
+  const response = await apiClient.get('me');
+
+  console.log(response.data);
+
+  return {
+    props: {},
+  };
+});
