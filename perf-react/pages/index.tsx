0b6c472df12c1d1fce97b90efc9fc001dd0df2cb
João Vítor <joaovsoutosantos@gmail.com>
2021-09-15 19:16:26 -0300

    Search for shirts data

--- perf-react/pages/index.tsx
+++ perf-react/pages/index.tsx
@@ -1,15 +1,43 @@
+import * as React from 'react';
 import type { NextPage } from 'next';
+
+import { SearchResults } from '../components/SearchResults';
+
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const [search, setSearch] = React.useState('');
+  const [results, setResults] = React.useState([]);
+
+  async function handleSearch(event: React.FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
+    <>
+      <h1 className={styles.title}>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          onChange={e => setSearch(e.target.value)}
+          value={search}
+        />
+
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </>
   );
 };
 
