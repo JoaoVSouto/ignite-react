b8e02609649b1067acca0916ade44d24bfded6cc
João Vítor <joaovsoutosantos@gmail.com>
2021-09-18 14:12:34 -0300

    Memoize total price value with useMemo

--- perf-react/components/SearchResults.tsx
+++ perf-react/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+
 import { ProductItem } from './ProductItem';
 
 type SearchResultsProps = {
@@ -9,8 +11,19 @@ type SearchResultsProps = {
 };
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = React.useMemo(
+    () =>
+      Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(results.reduce((total, product) => total + product.price, 0)),
+    [results]
+  );
+
   return (
     <div>
+      <h2>Total price: {totalPrice}</h2>
+
       {results.map(product => (
         <ProductItem key={product.id} product={product} />
       ))}
