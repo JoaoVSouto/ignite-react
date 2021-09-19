1c55f324b25f4e5eaa38190bb4ad6375ea9bf976
João Vítor <joaovsoutosantos@gmail.com>
2021-09-19 10:17:00 -0300

    Create AddProductToWishlist and import it dynamically

--- perf-react/components/ProductItem.tsx
+++ perf-react/components/ProductItem.tsx
@@ -1,4 +1,16 @@
 import * as React from 'react';
+import dynamic from 'next/dynamic';
+
+import type { AddProductToWishlistProps } from './AddProductToWishlist';
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
+  () => import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist),
+  {
+    loading: function LoadingAddProductToWishlist() {
+      return <div>Loading...</div>;
+    },
+  }
+);
 
 type ProductItemProps = {
   product: {
@@ -11,12 +23,20 @@ type ProductItemProps = {
 };
 
 function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWishlist] = React.useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.priceFormatted}</strong>
-      <button type="button" onClick={() => onAddToWishlist(product.id)}>
-        Add to wishlist
+      <button type="button" onClick={() => setIsAddingToWishlist(true)}>
+        Adicionar aos favoritos
       </button>
+      {isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishlist(product.id)}
+          onRequestClose={() => setIsAddingToWishlist(false)}
+        />
+      )}
     </div>
   );
 }
