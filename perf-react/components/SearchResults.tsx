2589f544797af0947f1dcc68e69c8796c391529c
João Vítor <joaovsoutosantos@gmail.com>
2021-09-19 10:33:20 -0300

    Refactor shirt list to be a virtualized one

--- perf-react/components/SearchResults.tsx
+++ perf-react/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from 'react-virtualized';
+
 import { ProductItem } from './ProductItem';
 
 type SearchResultsProps = {
@@ -16,17 +18,32 @@ export function SearchResults({
   totalPrice,
   onAddToWishlist,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => (
+    <div key={key} style={style}>
+      <ProductItem product={results[index]} onAddToWishlist={onAddToWishlist} />
+    </div>
+  );
+
   return (
     <div>
       <h2>Total price: {totalPrice}</h2>
 
-      {results.map(product => (
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map(product => (
         <ProductItem
           key={product.id}
           product={product}
           onAddToWishlist={onAddToWishlist}
         />
-      ))}
+      ))} */}
     </div>
   );
 }
